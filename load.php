d57245d4c1b7fa0d3effbb4af4e4300a33618d19
Ben Allfree <ben@softwareato.com>
2012-08-17 06:28:44 -0700

    Modified codegen to be dependent on config variable

--- load.php
+++ load.php
@@ -2,5 +2,8 @@
 
 W::lazyload_add_path($config['cache_fpath']);
 
-require('codegen.php');
+if($config['should_codegen'])
+{
+  require('codegen.php');
+}
 require($config['cache_fpath']."/config.php");
